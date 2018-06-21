0f1d3b684fab26292adaa4c89dcd1ec743a68c66
wxq <1798787619@qq.com>
2018-06-21 11:31:47 +0800

    图片验证码

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -15,9 +15,36 @@ var vm = new Vue({
 		mobile: '', 
 		image_code: '',
 		sms_code: '',
-		allow: false
+		allow: false,
+		image_code_url:'',
+		image_code_id:'',
 	},
+	mounted: function(){
+		this.generate_image_code()
+	}
+	,
 	methods: {
+		 generate_uuid: function(){
+        var d = new Date().getTime();
+        if(window.performance && typeof window.performance.now === "function"){
+            d += performance.now(); //use high-precision timer if available
+        }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c =='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    },
+		//生成图片验证码url
+		generate_image_code:function(){
+		 	//生成图片验证码
+		this.image_code_id=this.generate_uuid()
+		//组装图片验证码url地址
+		this.image_code_url="http://127.0.0.1:8000/image_code/"+this.image_code_id
+
+		},
+
 		check_username: function (){
 			var len = this.username.length;
 			if(len<5||len>20) {
